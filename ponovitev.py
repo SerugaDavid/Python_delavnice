8410de7a1abdbb5beca7472dba193df368a3aedd
davidseruga <56600501+davidseruga@users.noreply.github.com>
2022-01-28 17:40:05 +0100

    Initial commit

---
+++ ponovitev.py
@@ -0,0 +1,21 @@
+# KAJ SMO SE DO SEDAJ NAUČILI?
+
+# RAČUNANJE IN OPERATORJI
+# + seštevanje
+print(2 + 3)  # 5
+# - odštevanje
+print(3 - 2)  # 1
+# * množenje
+print(2 * 3)  # 6
+# / deljenje
+print(5 / 2)  # 2.5
+#
+# nekaj dodatnih operatorjev
+# // celoštevilčno deljenje
+print(5 / 2)  # 2
+# ** potenciranje
+print(5 ** 2)  # 25
+# % ostanek pri deljenju
+print(5 % 2)  # 1
+
+
