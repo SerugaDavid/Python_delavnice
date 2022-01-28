50eadf0fa9ce5b1d8d7964dd75e4ae44e61403f9
davidseruga <56600501+davidseruga@users.noreply.github.com>
2022-01-28 18:26:03 +0100

    Priprava na delavnice

--- ponovitev.py
+++ ponovitev.py
@@ -12,10 +12,48 @@ print(5 / 2)  # 2.5
 #
 # nekaj dodatnih operatorjev
 # // celoštevilčno deljenje
-print(5 / 2)  # 2
+print(5 // 2)  # 2
 # ** potenciranje
 print(5 ** 2)  # 25
 # % ostanek pri deljenju
 print(5 % 2)  # 1
 
+# BESEDILO
+print("Navadno besedilo")  # besedilo z dvojnimi narekovaji "
+print('Navadno besedilo')  # besedilo z enojnimi narekovaji '
+print("Želimo 'uporabiti' enojne narekovaje v besedilu")  # potrebujemo, da jih obkrožijo dvojni narekovaji "
+print('Želimo "uporabiti" dvojne narekovaje v besedilu')  # potrebujemo, da jih obkrožijo enojni narekovaji '
+print("""Želimo uporabiti "dvojne" in 'enojne' narekovaje""")  # potrebujemo, da jih obkrižijo 3x dvojni ali enojni narekovaji """/'''
+print("""To je prva vrsta,
+to je druga vrsta,
+to je tretja vrsta""")  # 3x dvojni narekovaji nam tudi omogočijo da pišemo več vrstično besedilo
+print("Vrsta 1\nVrsta 2\nVrsta 3")  # prav tako nam novo vrsto omogoči znak \n
+
+# LOGIČNI IZRAZI
+# > večje
+print(5 > 2)  # True
+# < manjše
+print(5 < 2)  # False
+# == je enako
+print(5 == 2)  # False
+# != ni enako
+print(5 != 2)  # True
+# >= večje ali enako
+print(5 >= 2)  # True
+# <= manjše ali enako
+print(5 <= 2)  # False
+
+# SPREMENLJIVKE
+# spremenljivke lahko poimenujemo kakorkoli
+a = 5
+b = 5
+stevilka = 5
+ksjdfksdnfgnasf = 5
+# spremenljivke imajo lahko različne podatkovne tipe
+a = 5
+a = True
+a = "pet"
+# s spremenljivkami lahko delamo, kot bi delali z navadnimi podatki
+print(stevilka + b)  # 10
+print(a == ksjdfksdnfgnasf)  # False
 
