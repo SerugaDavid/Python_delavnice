f74a39274c58e284aa9e1f21474c3a142d95167d
David <53861457+SerugaDavid@users.noreply.github.com>
2022-01-31 14:18:16 +0100

    rešene naloge

---
+++ Vaja 1/NalogeResitve.py
@@ -0,0 +1,27 @@
+print("Naloga 1")
+besedilo = input("Vnesi besedilo: ")
+if besedilo:
+    print(besedilo)
+else:
+    print("Uporabnik ni vnesel besedila!")
+
+print("\nNaloga 2")
+prijatelji = int(input("Koliko prijateljev ima Muri?: "))
+if prijatelji:
+    print("Vsakemu prijatelju daj: " + str(10/prijatelji) + "€")
+else:
+    print("Muri, obdrži svojih 10€!")
+
+povprecje = float(input("Kakšno imaš povprečje?: "))
+if povprecje < 1.5:
+    print("Nezadostno")
+elif povprecje < 2.5:
+    print("zadostno")
+elif povprecje < 3.5:
+    print("dobro")
+elif povprecje < 4.5:
+    print("prav dobro")
+elif povprecje <= 5:
+    print("odlično")
+else:
+    print("napačno povprečje")
