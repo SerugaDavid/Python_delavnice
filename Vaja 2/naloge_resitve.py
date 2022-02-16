9b3737ffb55464763b821031162c5e0882479bf0
David <53861457+SerugaDavid@users.noreply.github.com>
2022-02-16 11:13:48 +0100

    resitve nalog

---
+++ Vaja 2/naloge_resitve.py
@@ -0,0 +1,27 @@
+
+
+# print("Naloga 1")
+# vnos = int(input("Vnesi številko: "))
+# stevec = 1
+# while stevec <= vnos:
+#     print(stevec)
+#     stevec += 1
+
+# print("Naloga 2")
+# besedilo = input("Vnesi besedilo, ki ga želiš ponavljati: ")
+# ponovitve = int(input("Vnesi kolikokrat želiš besedilo ponoviti: "))
+# ponv2 = ponovitve
+# while ponovitve >= 1:
+#     print(besedilo)
+#     ponovitve -= 1
+# print("konec")
+# besedilo += "\n"
+# print(besedilo*ponv2)
+
+print("Naloga 3")
+vnos = 1
+sestevek = 0
+while vnos:
+    vnos = float(input("Vnesi številko: "))
+    sestevek += vnos
+print(sestevek)
